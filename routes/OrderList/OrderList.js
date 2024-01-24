444c4eddc9b7e6f1515bd5265fa6b5ad4cea97de
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-01-24 22:30:14 +0530

    grouped customer for frontend

--- routes/OrderList/OrderList.js
+++ routes/OrderList/OrderList.js
@@ -14,11 +14,16 @@ OrderListRouter.post(`/getOrderListByType`, async (req, res, next) => {
   try {
     misQueryMod(
       `SELECT 
-                *
-            FROM
-                magodmis.order_list
-            WHERE
-                Type = '${req.body.type}'`,
+            *,
+            DATE_FORMAT(Order_Date, '%d/%m/%Y') AS Printable_Order_Date,
+            DATE_FORMAT(Delivery_Date, '%d/%m/%Y') AS Printable_Delivery_Date
+        FROM
+            magodmis.order_list
+                INNER JOIN
+            magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
+        WHERE
+            Type = '${req.body.type}' AND Order_Status = 'Closed'
+        ORDER BY Order_Date DESC`,
       (err, data) => {
         if (err) {
           console.log("err", err);
@@ -32,5 +37,35 @@ OrderListRouter.post(`/getOrderListByType`, async (req, res, next) => {
     next(error);
   }
 });
+OrderListRouter.post(
+  `/getOrderListByTypeGroupedCustomer`,
+  async (req, res, next) => {
+    //   console.log("reqqq", req.body.type);
+    try {
+      misQueryMod(
+        `SELECT 
+            magodmis.cust_data.*
+        FROM
+            magodmis.order_list
+                INNER JOIN
+            magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
+        WHERE
+            Type = '${req.body.type}' AND Order_Status = 'Closed'
+        GROUP BY magodmis.cust_data.Cust_Code
+        ORDER BY magodmis.cust_data.Cust_name`,
+        (err, data) => {
+          if (err) {
+            console.log("err", err);
+          } else {
+            //   console.log("data", data);
+            res.send(data);
+          }
+        }
+      );
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = OrderListRouter;
