eb5329ef773717ff9b1cac1cedc0c76dcccd66ba
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-01-28 16:45:15 +0530

    filtering order sattus

--- routes/OrderList/OrderList.js
+++ routes/OrderList/OrderList.js
@@ -9,27 +9,40 @@ const {
 } = require("../../helpers/dbconn");
 
 OrderListRouter.post(`/getOrderListByType`, async (req, res, next) => {
+  let query = `SELECT 
+                  *,
+                  DATE_FORMAT(Order_Date, '%d/%m/%Y') AS Printable_Order_Date,
+                  DATE_FORMAT(Delivery_Date, '%d/%m/%Y') AS Printable_Delivery_Date
+                FROM
+                  magodmis.order_list
+                      INNER JOIN
+                  magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
+                WHERE
+                  Type = '${req.body.type}'
+                ORDER BY Order_Date DESC`;
+
+  if (req.body.Order_Status !== "All") {
+    query = `SELECT 
+                *,
+                DATE_FORMAT(Order_Date, '%d/%m/%Y') AS Printable_Order_Date,
+                DATE_FORMAT(Delivery_Date, '%d/%m/%Y') AS Printable_Delivery_Date
+              FROM
+                magodmis.order_list
+                    INNER JOIN
+                magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
+              WHERE
+                Type = '${req.body.type}' AND Order_Status = '${req.body.Order_Status}'
+              ORDER BY Order_Date DESC`;
+  }
+
   try {
-    misQueryMod(
-      `SELECT 
-            *,
-            DATE_FORMAT(Order_Date, '%d/%m/%Y') AS Printable_Order_Date,
-            DATE_FORMAT(Delivery_Date, '%d/%m/%Y') AS Printable_Delivery_Date
-        FROM
-            magodmis.order_list
-                INNER JOIN
-            magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
-        WHERE
-            Type = '${req.body.type}'
-        ORDER BY Order_Date DESC`,
-      (err, data) => {
-        if (err) {
-          console.log("err", err);
-        } else {
-          res.send(data);
-        }
+    misQueryMod(query, (err, data) => {
+      if (err) {
+        console.log("err", err);
+      } else {
+        res.send(data);
       }
-    );
+    });
   } catch (error) {
     next(error);
   }
@@ -37,26 +50,38 @@ OrderListRouter.post(`/getOrderListByType`, async (req, res, next) => {
 OrderListRouter.post(
   `/getOrderListByTypeGroupedCustomer`,
   async (req, res, next) => {
+    let query = `SELECT 
+                    magodmis.cust_data.*
+                  FROM
+                    magodmis.order_list
+                        INNER JOIN
+                    magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
+                  WHERE
+                    Type = '${req.body.type}'
+                  GROUP BY magodmis.cust_data.Cust_Code
+                  ORDER BY magodmis.cust_data.Cust_name`;
+
+    if (req.body.Order_Status !== "All") {
+      query = `SELECT 
+                  magodmis.cust_data.*
+                FROM
+                  magodmis.order_list
+                      INNER JOIN
+                  magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
+                WHERE
+                  Type = '${req.body.type}' AND Order_Status = '${req.body.Order_Status}'
+                GROUP BY magodmis.cust_data.Cust_Code
+                ORDER BY magodmis.cust_data.Cust_name`;
+    }
+
     try {
-      misQueryMod(
-        `SELECT 
-            magodmis.cust_data.*
-        FROM
-            magodmis.order_list
-                INNER JOIN
-            magodmis.cust_data ON magodmis.order_list.Cust_Code = magodmis.cust_data.Cust_Code
-        WHERE
-            Type = '${req.body.type}'
-        GROUP BY magodmis.cust_data.Cust_Code
-        ORDER BY magodmis.cust_data.Cust_name`,
-        (err, data) => {
-          if (err) {
-            console.log("err", err);
-          } else {
-            res.send(data);
-          }
+      misQueryMod(query, (err, data) => {
+        if (err) {
+          console.log("err", err);
+        } else {
+          res.send(data);
         }
-      );
+      });
     } catch (error) {
       next(error);
     }
