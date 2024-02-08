ec20617387e191f51fcab1c8b1ce42314234aa74
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-02-08 12:20:31 +0530

    fetched the data of profarma inv main and details...adi 8feb24

--- routes/OrderRoutes/ProfarmaInvList.js
+++ routes/OrderRoutes/ProfarmaInvList.js
@@ -1,8 +1,45 @@
 const ProfarmaInvListRouter = require("express").Router();
 const { misQuery, setupQuery, misQueryMod } = require("../../helpers/dbconn");
 
-ProfarmaInvListRouter.get("/test", async (req, res, next) => {
-  console.log("test");
+ProfarmaInvListRouter.post("/getProfarmaMain", async (req, res, next) => {
+  try {
+    misQueryMod(
+      `SELECT * FROM magodmis.profarma_main where OrderNo = '${req.body.OrderNo}'`,
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+ProfarmaInvListRouter.post("/getProfarmaDetails", async (req, res, next) => {
+  try {
+    misQueryMod(
+      `SELECT 
+            *
+        FROM
+            magodmis.profarma_main
+                JOIN
+            magodmis.profarmadetails ON magodmis.profarma_main.ProfarmaID = magodmis.profarmadetails.ProfarmaID
+        WHERE
+            OrderNo = '${req.body.OrderNo}'`,
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = ProfarmaInvListRouter;
