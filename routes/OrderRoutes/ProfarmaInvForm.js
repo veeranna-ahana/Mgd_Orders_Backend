e8db055ca4364ccdfcf13f6d89d0f5d230b3837f
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2024-02-10 21:13:02 +0530

    save invoice api done...adi 10feb24

--- routes/OrderRoutes/ProfarmaInvForm.js
+++ routes/OrderRoutes/ProfarmaInvForm.js
@@ -81,4 +81,125 @@ ProfarmaInvFormRouter.post("/getProfarmaFormTaxes", async (req, res, next) => {
     next(error);
   }
 });
+
+ProfarmaInvFormRouter.post("/postSaveInvoice", async (req, res, next) => {
+  // console.log("post save invoice", req.body);
+
+  // update main table
+
+  try {
+    misQueryMod(
+      `UPDATE magodmis.profarma_main
+      SET
+          TaxAmount = '${parseFloat(
+            req.body.profarmaMainData.TaxAmount || 0
+          ).toFixed(2)}',
+          Net_Total = '${parseFloat(
+            req.body.profarmaMainData.Net_Total || 0
+          ).toFixed(2)}',
+          Discount = '${parseFloat(
+            req.body.profarmaMainData.Discount || 0
+          ).toFixed(2)}',
+          AssessableValue = '${parseFloat(
+            req.body.profarmaMainData.AssessableValue || 0
+          ).toFixed(2)}',
+          Del_Chg = '${parseFloat(
+            req.body.profarmaMainData.Del_Chg || 0
+          ).toFixed(2)}',
+          InvTotal = '${parseFloat(
+            req.body.profarmaMainData.InvTotal || 0
+          ).toFixed(2)}',
+          Round_Off = '${parseFloat(
+            req.body.profarmaMainData.Round_Off || 0
+          ).toFixed(2)}',
+          GrandTotal = '${parseFloat(
+            req.body.profarmaMainData.GrandTotal || 0
+          ).toFixed(2)}'
+      WHERE
+          (ProfarmaID = '${req.body.profarmaMainData.ProfarmaID}')`,
+      (err, udpateMainData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // update details
+          for (let i = 0; i < req.body.profarmaDetailsData.length; i++) {
+            const element = req.body.profarmaDetailsData[i];
+
+            try {
+              misQueryMod(
+                `UPDATE magodmis.profarmadetails SET Qty = '${parseInt(
+                  element.Qty || 0
+                )}', Unit_Rate = '${parseFloat(element.Unit_Rate || 0).toFixed(
+                  2
+                )}', DC_Srl_Amt = '${parseFloat(
+                  element.DC_Srl_Amt || 0
+                ).toFixed(2)}' WHERE (ProfarmaDetailID = '${
+                  element.ProfarmaDetailID
+                }')`,
+                (err, udpateDetailsData) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                  }
+                }
+              );
+            } catch (error) {
+              next(error);
+            }
+          }
+
+          // delete tax data
+          try {
+            misQueryMod(
+              `DELETE FROM magodmis.profarmataxtable WHERE (ProfarmaID = '${req.body.profarmaMainData.ProfarmaID}')`,
+              (err, data) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                }
+              }
+            );
+          } catch (error) {
+            next(error);
+          }
+
+          // insert tax data
+
+          for (let i = 0; i < req.body.profarmaTaxData.length; i++) {
+            const element = req.body.profarmaTaxData[i];
+
+            try {
+              misQueryMod(
+                `INSERT INTO magodmis.profarmataxtable (ProfarmaID, TaxID, Tax_Name, TaxOn, TaxableAmount, TaxPercent, TaxAmt) VALUES ('${
+                  element.ProfarmaID
+                }', '${element.TaxID}', '${element.Tax_Name}', '${
+                  element.TaxOn
+                }', '${parseFloat(element.TaxableAmount).toFixed(
+                  2
+                )}', '${parseFloat(element.TaxPercent).toFixed(
+                  2
+                )}', '${parseFloat(element.TaxAmt).toFixed(2)}')`,
+                (err, insertTaxData) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                  }
+                }
+              );
+            } catch (error) {
+              next(error);
+            }
+          }
+
+          res.send({
+            flag: true,
+            message: "Invoice saved successfully",
+          });
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = ProfarmaInvFormRouter;
