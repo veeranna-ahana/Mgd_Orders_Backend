e41fb6615685a369c9ba5c765aeb0af9988d148d
veeranna-ahana <bedasur.veeranna@ahana.co.in>
2024-07-03 13:06:26 +0530

    veer3724

--- routes/processlist.js
+++ routes/processlist.js
@@ -1,18 +1,22 @@
+/** @format */
+
 const processlistRouter = require("express").Router();
-var createError = require('http-errors');
+var createError = require("http-errors");
 
-const { misQuery } = require('../helpers/dbconn');
+const { misQuery } = require("../helpers/dbconn");
 
-processlistRouter.get('/allprocesslists', async (req, res, next) => {
-    try {
-        misQuery("Select * from magodmis.process_list order by ProcessDescription asc", (data) => {
-            res.send(data)
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+processlistRouter.get("/allprocesslists", async (req, res, next) => {
+	try {
+		// misQuery("Select * from magodmis.process_list order by ProcessDescription asc", (data) => {
+		misQuery(
+			"Select * from machine_data.magod_process_list order by ProcessDescription asc",
+			(data) => {
+				res.send(data);
+			}
+		);
+	} catch (error) {
+		next(error);
+	}
+});
 
-
-
-module.exports = processlistRouter
+module.exports = processlistRouter;
