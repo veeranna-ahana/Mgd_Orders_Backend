f586cdd02f05d22dff9f34f4c4c2ff9ae2a19b19
Prakruthi Holla <prakruthi.holla@ahanait.com>
2024-07-05 10:12:36 +0530

    backend updated on 05/07/2024

--- routes/CombinedSchedule/scheduleListCombined.js
+++ routes/CombinedSchedule/scheduleListCombined.js
@@ -177,12 +177,11 @@ scheduleListCombined.post('/OriginalTable2', jsonParser, async (req, res, next) 
 
 //Onclick of Update To Original Schedule
 scheduleListCombined.post('/updateToOriganalSchedule', jsonParser, async (req, res, next) => {
-    // console.log("req.body", req.body);
     try {
         const scheduleId = req.body.selectedRow.ScheduleId;
         
         // Query to fetch rows from orderscheduledetails
-        const ordersQuery = `SELECT * FROM magodmis.orderscheduledetails WHERE ScheduleId=?;`;
+        const ordersQuery = `SELECT * FROM magodmis.orderscheduledetails WHERE ScheduleId='${scheduleId}';`;
         const ordersResult = await mchQueryMod1(ordersQuery, [scheduleId]);
 
         // Loop through each row in the result
@@ -190,8 +189,9 @@ scheduleListCombined.post('/updateToOriganalSchedule', jsonParser, async (req, r
             const schDetailsId = orderRow.SchDetailsID;
 
             // Query to fetch rows from combined_schedule_part_details
-            const combinedQuery = `SELECT * FROM magodmis.combined_schedule_part_details WHERE N_SchDetailsID=?;`;
+            const combinedQuery = `SELECT * FROM magodmis.combined_schedule_part_details WHERE N_SchDetailsID='${schDetailsId}';`;
             const combinedResult = await mchQueryMod1(combinedQuery, [schDetailsId]);
+
 
             // Loop through each row in the combinedResult
             for (const combinedRow of combinedResult) {
@@ -210,15 +210,19 @@ scheduleListCombined.post('/updateToOriganalSchedule', jsonParser, async (req, r
                         t.QtyProduced = t.QtyProduced + (c.QtyCleared - c.QtyDistributed),
                         t.QtyCleared = t.QtyCleared + (c.QtyCleared - c.QtyDistributed)
                     WHERE 
-                        c.cmbSchId = ? AND t.SchDetailsId = c.O_SchDetailsID AND c.cmbSchPartID = ?;
+                        c.cmbSchId = '${cmbSchId}' AND t.SchDetailsId = c.O_SchDetailsID AND c.cmbSchPartID = '${cmbSchPartID}';
                 `;
+
+
 
                 // Update query for setting QtyDistributed in combined_schedule_part_details
                 const updateQtyDistributedQuery = `
                     UPDATE magodmis.combined_schedule_part_details c 
                     SET c.QtyDistributed = c.QtyCleared 
-                    WHERE c.cmbSchPartID = ?;
+                    WHERE c.cmbSchPartID ='${cmbSchPartID}' ;
                 `;
+
+
 
                 // Update query for task_partslist and orderscheduledetails
                 const updateTaskOrdersQuery = `
@@ -230,8 +234,9 @@ scheduleListCombined.post('/updateToOriganalSchedule', jsonParser, async (req, r
                         o.Part_Area = t.Part_Area, 
                         o.UnitWt = t.Unit_Wt 
                     WHERE 
-                        o.ScheduleId = ? AND o.SchDetailsId = t.SchDetailsId;
+                        o.ScheduleId = '${ScheduleID}'  AND o.SchDetailsId = t.SchDetailsId;
                 `;
+
 
                 // Execute the update queries with parameters
                 await mchQueryMod1(updateCombinedQuery, [cmbSchId, cmbSchPartID]);
@@ -351,9 +356,73 @@ scheduleListCombined.post('/updateTask', jsonParser, async (req, res, next) => {
   });
   
   
+  //Print
+  scheduleListCombined.post(`/PrintPdf`, async (req, res, next) => {
+    console.log("req.body.selectedRow[0].ScheduleId is",req.body.formdata.ScheduleId);
+    try {
+      let query = `SELECT * FROM magodmis.orderscheduledetails where ScheduleId='${req.body.formdata.ScheduleId}';`;
   
+      misQueryMod(query, (err, data) => {
+        if (err) {
+          console.log("err", err);
+          res
+            .status(500)
+            .send({ error: "An error occurred while fetching data" });
+        } else {
+          if (data.length > 0) {
+            // Group data by task number
+            const groupedData = {};
+            data.forEach((item) => {
+              const TaskNo = item.TaskNo;
+              if (!groupedData[TaskNo]) {
+                groupedData[TaskNo] = [];
+              }
+              groupedData[TaskNo].push(item);
+            });
   
+            // Format grouped data
+            const formattedData = [];
+            for (const TaskNo in groupedData) {
+              formattedData.push({
+                taskNo: TaskNo,
+                Mtrl_Code: groupedData[TaskNo][0].Mtrl_Code,
+                Mtrl_Source: groupedData[TaskNo][0].Mtrl_Source,
+                Operation: groupedData[TaskNo][0].Operation,
+                otherdetails: groupedData[TaskNo],
+              });
+            }
   
+            res.send(formattedData);
+          } else {
+            res
+              .status(404)
+              .send({ error: "No data found for the provided ScheduleId" });
+          }
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+
+  //get customer name
+  scheduleListCombined.post(`/getCustomerNamePDF`, async (req, res, next) => {
+    let query = `SELECT Cust_name FROM magodmis.cust_data  where Cust_Code='${req.body.formdata.Cust_Code}'
+    `;
+  
+    try {
+      misQueryMod(query, (err, data) => {
+        if (err) {
+          console.log("err", err);
+        } else {
+          res.send(data);
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
   
   
